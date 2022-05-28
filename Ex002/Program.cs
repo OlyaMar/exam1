7078dc86a6c6c079ed483d1af408e7384ef4a8ac
Olya <olya.markelova@gmail.com>
2022-05-28 17:56:11 +0300

    1 задача

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Olya");
+﻿
+Console.WriteLine("Введите 2 числа");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+int b = Convert.ToInt32(Console.ReadLine()); 
+if (a * a == b)
+{
+    Console.WriteLine("квадратный корень");
+}
+else
+{
+    Console.WriteLine("Не квадратный корень");
+}
+
